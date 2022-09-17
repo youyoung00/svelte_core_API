711c40db46534f7bb7efcf84b866540832d1da15
youyoung00 <you889@hanmail.net>
2022-09-17 12:21:05 +0900

    tick 테스트

---
+++ tick/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import {tick} from 'svelte'
+	let name = 'world'
+	async function handler(){
+		name = 'Shin'
+		await tick()
+		const h1 = document.querySelector('h1')
+		console.log(h1.innerText)
+	}
+</script>
+
+<h1 on:click={handler}>Hello {name}!</h1>
