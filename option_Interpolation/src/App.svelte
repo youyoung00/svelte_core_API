c6643679a28287cda689554800d64b535f745d9d
youyoung00 <you889@hanmail.net>
2022-09-17 13:03:03 +0900

    보간법/ 내용 / 속성 / 표현식

---
+++ option_Interpolation/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	let href = 'https://heropy.blog'
+	let name = 'Herropy'
+	let value = 'New input value!'
+	let isUpperCase = false
+
+</script>
+
+<!-- <a href="https://heropy.blog">Herropy</a> -->
+<a {href}>{name}</a>
+
+<!-- <input type="text" value="Default value.."> -->
+<input {value} on:input={e => value = e.target.value}>
+<input bind:value />
+
+<div>{isUpperCase ? 'DIV' : 'div'}</div>
