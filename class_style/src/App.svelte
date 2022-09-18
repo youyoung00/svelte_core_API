a28a82511c0510b5300dfe17ffc289d718cc6983
youyoung00 <you889@hanmail.net>
2022-09-18 18:24:54 +0900

    스벨트의 기본 클래스 속성 바인딩

---
+++ class_style/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	let active = false
+</script>
+
+<button on:click={() => {active = !active}}>
+	Toggle!
+</button>
+
+<div class={active ? 'active' : ''}>
+	Hello
+</div>
+
+<style>
+	div {
+		width: 120px;
+		height: 200px;
+		background: royalblue;
+		border-radius: 10px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: white;
+		transition: .4s;
+	}
+	.active{
+		width: 250px;
+		background: tomato;
+	}
+</style>
